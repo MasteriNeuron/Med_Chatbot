e822cc24c4e856da9a0cac69b59875df286d87a6
Master_Neuron <tcsshubham5@gmail.com>
2025-10-03 10:37:34 +0530

    ys

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Master_Neuron",
+    author_email="shubham.chaudhary@pw.live",
+    packages=find_packages(),
+    install_requires=[]
+)
