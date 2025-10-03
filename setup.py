149f97c7e6d858254a561fab4ae51ac6ef48513d
Master_Neuron <tcsshubham5@gmail.com>
2025-10-03 15:41:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name="medical_chatbot",
     version="0.1.0",
     author="Master_Neuron",
-    author_email="shubham.chaudhary@pw.live",
+    author_email="drsnchaudhary",
     packages=find_packages(),
     install_requires=[]
+
 )
